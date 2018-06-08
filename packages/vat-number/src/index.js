30d400ea52ca36d5c56e898a2fd9f7aa59d5c54e
Adrian Tombu <adrian@otso.fr>
2018-06-08 10:19:34 +0200

    Testing & refactoring & travis deployment

--- packages/vat-number/src/index.js
+++ packages/vat-number/src/index.js
@@ -2,25 +2,25 @@
 
 const soap = require('soap')
 
-module.exports = async function (vatNumber) {
-  const url = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
+module.exports = async vatNumber => {
+  const apiUrl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
   const args = {
     countryCode: vatNumber.substring(0, 2),
     vatNumber: vatNumber.substring(2)
   }
 
-  const client = await soap.createClientAsync(url)
-  const result = await client.checkVatAsync(args)
+  try {
+    const client = await soap.createClientAsync(apiUrl)
+    const result = await client.checkVatAsync(args)
 
-  if (!result || !result.valid) {
-    return null
-  }
-
-  return {
-    country: result.countryCode,
-    vatNumber: result.vatNumber,
-    fullVatNumber: `${result.countryCode}${result.vatNumber}`,
-    name: result.name,
-    address: result.address
+    return {
+      country: result.countryCode,
+      vatNumber: result.vatNumber,
+      fullVatNumber: `${result.countryCode}${result.vatNumber}`,
+      name: result.name,
+      address: result.address
+    }
+  } catch (err) {
+    throw new Error('The VAT number provided is not valid')
   }
 }
---
+++ packages/vat-number/src/index.test.js
@@ -0,0 +1,18 @@
+const getVatNumberInfos = require('./index')
+
+it('the processed vat should return a proper object', async () => {
+  const vatNumber = 'FR16817871668'
+  const data = await getVatNumberInfos(vatNumber)
+
+  expect(data).toHaveProperty('country', 'FR')
+  expect(data).toHaveProperty('vatNumber', '16817871668')
+  expect(data).toHaveProperty('fullVatNumber', 'FR16817871668')
+  expect(data).toHaveProperty('name', 'SASU OTSO')
+  expect(data).toHaveProperty('address', 'CS 21531\n59 ALL JEAN JAURES\n31000 TOULOUSE')
+})
+
+it('a wrong vat number should return an error', async () => {
+  const vatNumber = 'XX99999999999'
+
+  await expect(getVatNumberInfos(vatNumber)).rejects.toThrow()
+})
