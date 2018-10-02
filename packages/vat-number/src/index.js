f1f62a15285eb7fdfb8d24191b3f80144e750624
Adrian Tombu <adriantombu@users.noreply.github.com>
2018-10-02 09:26:57 +0200

    Merge pull request #10 from adriantombu/feature/npm-updates
    NPM security updates

--- packages/vat-number/src/index.js
+++ packages/vat-number/src/index.js
@@ -14,11 +14,11 @@ module.exports = async vatNumber => {
     const result = await client.checkVatAsync(args)
 
     return {
-      country: result.countryCode,
-      vatNumber: result.vatNumber,
-      fullVatNumber: `${result.countryCode}${result.vatNumber}`,
-      name: result.name,
-      address: result.address
+      country: result[0].countryCode,
+      vatNumber: result[0].vatNumber,
+      fullVatNumber: `${result[0].countryCode}${result[0].vatNumber}`,
+      name: result[0].name,
+      address: result[0].address
     }
   } catch (err) {
     throw new Error('The VAT number provided is not valid')
