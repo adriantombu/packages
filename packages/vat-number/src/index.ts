a99fcf4e2bc355739b8b41bdda85e6ff97a11b2c
Adrian Tombu <adriantombu@users.noreply.github.com>
2019-11-17 13:18:22 +0100

    Merge pull request #35 from adriantombu/fix/npm-deploy
    🐛 Fixes NPM deploy

--- packages/vat-number/src/index.ts
+++ packages/vat-number/src/index.ts
@@ -4,7 +4,7 @@ export async function getVatNumberInfos(vatNumber: string): Promise<Result> {
   const apiUrl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
   const args = {
     countryCode: vatNumber.substring(0, 2),
-    vatNumber: vatNumber.substring(2)
+    vatNumber: vatNumber.substring(2),
   }
 
   try {
@@ -17,23 +17,23 @@ export async function getVatNumberInfos(vatNumber: string): Promise<Result> {
       fullVatNumber: `${results[0].countryCode}${results[0].vatNumber}`,
       name: results[0].name,
       address: results[0].address,
-      valid: true
+      valid: true,
     }
   } catch (err) {
     return {
       fullVatNumber: vatNumber,
       message: 'The VAT number provided is not valid',
-      valid: false
+      valid: false,
     }
   }
 }
 
 interface Result {
   country?: string
-  vatNumber?: string,
-  fullVatNumber: string,
-  message?: string,
-  name?: string,
-  address?: string,
+  vatNumber?: string
+  fullVatNumber: string
+  message?: string
+  name?: string
+  address?: string
   valid: boolean
 }
