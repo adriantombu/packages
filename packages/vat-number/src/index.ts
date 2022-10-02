4bd3f9e51a5918fef8ab095d32ffeb163ef1cb7b
Adrian Tombu <adrian@otso.fr>
2022-10-02 09:23:16 +0000

    🐛 Fixes vat-number test + upgrade dependencies

--- packages/vat-number/src/index.ts
+++ packages/vat-number/src/index.ts
@@ -1,7 +1,7 @@
 import * as soap from 'soap'
 
 export async function getVatNumberInfos(vatNumber: string): Promise<Result> {
-  const apiUrl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
+  const apiUrl = 'https://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
   const args = {
     countryCode: vatNumber.substring(0, 2),
     vatNumber: vatNumber.substring(2),
@@ -14,7 +14,7 @@ export async function getVatNumberInfos(vatNumber: string): Promise<Result> {
     return {
       country: results[0].countryCode,
       vatNumber: results[0].vatNumber,
-      fullVatNumber: `${results[0].countryCode}${results[0].vatNumber}`,
+      fullVatNumber: `${results[0].countryCode}${results[0].vatNumber || args.vatNumber}`,
       name: results[0].name,
       address: results[0].address,
       valid: results[0].valid,
