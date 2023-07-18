f6928bc7061ef7487b2bc752053f636f72d18c5e
Adrian Tombu <adrian@otso.fr>
2023-07-18 13:04:58 +0000

    chore: upgrade dependencies

--- packages/paybox-system/jest.config.js
+++ packages/paybox-system/jest.config.js
@@ -1,33 +1,18 @@
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  testEnvironment: "node",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  roots: [
-    "<rootDir>/src"
-  ],
-  collectCoverageFrom: [
-    "src/**/*.ts"
-  ],
-  coverageReporters: [
-    "html",
-    "text",
-    "lcov"
-  ],
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageReporters: ['html', 'text', 'lcov'],
   transform: {
-    "^.+\\.ts$": "ts-jest"
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true
-    }
-  }
-};
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
